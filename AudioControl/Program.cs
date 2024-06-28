2ab47875682422b1b7128f54b57f03af4e10e424
GitHub <markus.wiest@protonmail.com>
2024-06-28 22:37:05 +0200

    Arduino comunication

--- AudioControl/Program.cs
+++ AudioControl/Program.cs
@@ -1,25 +1,78 @@
-﻿using System;
-
+﻿using System.IO.Ports;
 using NAudio.CoreAudioApi;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
-internal class Program
+
+public static class SerializeAndDeserialize
 {
-    private static void Main(string[] args)
+    public static string Serialize<T>(T obj)
     {
-        var enumerator = new MMDeviceEnumerator();
-        foreach (var endpoint in enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
-        {
-            //Console.WriteLine(endpoint.FriendlyName);
-            //Console.WriteLine(endpoint.ID);
-            //Console.WriteLine(endpoint.AudioEndpointVolume.MasterVolumeLevel);
-            //Console.WriteLine(endpoint.AudioEndpointVolume.MasterVolumeLevelScalar);
-            //Console.WriteLine(endpoint.AudioEndpointVolume.Mute);
+        var serializer = new SerializerBuilder().Build();
+        return serializer.Serialize(obj);
+    }
+    public static T Deserialize<T>(string yaml)
+    {
+        var deserializer = new DeserializerBuilder().Build();
+        return deserializer.Deserialize<T>(yaml);
+    }
+}
 
-            if (endpoint.ID == "{0.0.0.00000000}.{a3a272e2-8b47-41ec-8b55-95010bd41a90}") {
-                Console.WriteLine(endpoint.FriendlyName);
-                float volume = 0.1F;
-                endpoint.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+class Program
+{
+
+    public SerialPort serialPort = new SerialPort();
+    public MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+
+    public static void Main()
+                => new Program().MainInterface();
+
+    public void MainInterface()
+    {
+
+        readMapFile(@"C:\Users\Schre\Documents\IdeaProjects\AudioControl\AudioControl\config.yaml");
+
+        var portNames = SerialPort.GetPortNames();
+        foreach (var port in portNames)
+        {
+            if (port == "COM8")
+            {
+                serialPort.PortName = "COM8";
+                serialPort.BaudRate = 9600;
+                serialPort.DtrEnable = true;
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataRecieved);
+                serialPort.Open();
+
+                Console.ReadKey();
+                serialPort.Close();
+
             }
         }
+    }
+
+    public void SerialDataRecieved(object sender, SerialDataReceivedEventArgs e)
+    {
+        string inData = serialPort.ReadLine();
+        Console.WriteLine($"Data Received: {inData}");
+
+    }
+
+    public void ApplySoundChanges(String id, float value, bool mute)
+    {
+        foreach (var endpoint in enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
+        {
+            if (endpoint.ID == id)
+            {
+                Console.WriteLine(endpoint.FriendlyName);
+                float volume = value;
+                endpoint.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+                endpoint.AudioEndpointVolume.Mute = mute;
+            }
+        }
+    }
+
+    public void readMapFile(string filepath)
+    {
+
     }
 }
